4111fb58ddb8a8b89707ed5acaa7ffd7fedc88a6
Sophia Carrasco Obregon <sophiaa1carrascoo@gmail.com>
2025-10-16 15:55:34 -0700

    added two new columns, that we discussed with Dr. Sun

--- metadata/Editing_metadata_files.R
+++ metadata/Editing_metadata_files.R
@@ -9,39 +9,68 @@ library(readxl)
 #wher eis determines if the subject consumes only "Ecigarretes", only "Tobacco", 
 #or "None", any other possibility is NA 
 
+##Oct 16th -> adding two new columns, one for cohort and another one for 'combined'
+# which will be 'cohort_description'
 
-## Editing the vaping_uk_meatadata
 
-meta_uk_fp <- "vaping_uk_metadata.xlsx"
-meta_uk <- read_excel(meta_uk_fp)
 
-## Creating a new column, looking at the "Ecig" and "Tobacco" conditons
-meta_uk_new <- meta_uk %>%
+
+#### Editing the vaping_uk_meatadata ####
+
+meta_us_fp <- "metadata/vaping_uk_metadata.xlsx"
+meta_us <- read_excel(meta_us_fp)
+
+## Creating a new column, looking at the "ecig" and "tobacco" conditons
+meta_us_description_column <- meta_us %>%
   mutate(Description = case_when(
-    Ecig == "No" & Tobacco == "No" ~ "None",
-    Ecig == "Yes" & Tobacco == "No" ~ "Ecigarettes",
-    Ecig == "No" & Tobacco == "Yes" ~ "Tobacco",
+    Ecig == "No" & Tobacco == "No" ~ "control",
+    Ecig == "Yes" & Tobacco == "No" ~ "ecigarettes",
+    Ecig == "No" & Tobacco == "Yes" ~ "tobacco",
     TRUE ~ NA_character_  
   ))
 
-## Editing the vaping_chinese_meatadata
+## Creating a new column, for cohort--> all will be us 
+meta_us_description_cohort_columns <- meta_us_description_column %>%
+  mutate(Cohort = "us")
 
-meta_cn_fp <- "vaping_chinese_metadata.xlsx"
+## Creating a new column combining the two new columns
+meta_us_description_cohort_combined_columns <- meta_us_description_cohort_columns %>%
+  mutate (Combined = paste(Cohort, Description, sep = "_"))
+
+##Renaming the final metadata file
+meta_us_new <- meta_us_description_cohort_combined_columns
+  
+  
+
+
+#### Editing the vaping_chinese_meatadata ####
+
+meta_cn_fp <- "metadata/vaping_chinese_metadata.xlsx"
 meta_cn <- read_excel(meta_cn_fp)
 
 ## Creating a new column, whith the same contidions as above
-meta_cn_new <- meta_cn  %>%
+meta_cn_description_column <- meta_cn  %>%
   mutate(Description = case_when(
-    str_detect(`Public description`, regex("E-cig", ignore_case = TRUE)) ~ "Ecigarettes",
-    str_detect(`Public description`, regex("Non", ignore_case = TRUE)) ~ "None",
-    str_detect(`Public description`, regex("Common", ignore_case = TRUE)) ~ "Tobacco",
+    str_detect(`Public description`, regex("E-cig", ignore_case = TRUE)) ~ "ecigarettes",
+    str_detect(`Public description`, regex("Non", ignore_case = TRUE)) ~ "control",
+    str_detect(`Public description`, regex("Common", ignore_case = TRUE)) ~ "tobacco",
     TRUE ~ NA_character_
   ))
 
+## Creating a new column, for cohort--> all will be us 
+meta_cn_description_cohort_columns <- meta_cn_description_column %>%
+  mutate(Cohort = "cn")
 
-## Save both files as cn_metadata_new and uk_metadata_new
+## Creating a new column combining the two new columns
+meta_cn_description_cohort_combined_columns <- meta_cn_description_cohort_columns %>%
+  mutate (Combined = paste(Cohort, Description, sep = "_"))
 
-write_tsv(meta_cn_new, "cn_metadata_new.tsv")
+##Renaming the final metadata file
+meta_cn_new <- meta_cn_description_cohort_combined_columns
 
-write_tsv(meta_uk_new, "uk_metadata_new.tsv")
+## Save both files as cn_metadata_new and us_metadata_new (the "uk" is actually from us volunteers)
+
+write_tsv(meta_cn_new, "metadata/cn_metadata_new.tsv")
+
+write_tsv(meta_us_new, "metadata/uk_metadata_new.tsv")
 

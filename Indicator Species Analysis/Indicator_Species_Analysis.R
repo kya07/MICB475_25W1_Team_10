58f458c6ffb34643d2ec9187e95638910954fed0
aimee-stackusername <aimee.bellefemme@gmail.com>
2025-11-10 11:43:04 -0800

    Modified ISA R script as well as the addition of the ISA final table

--- Indicator Species Analysis/Indicator_Species_Analysis.R
+++ Indicator Species Analysis/Indicator_Species_Analysis.R
@@ -8,76 +8,28 @@ library(indicspecies)
 #### Load the filtered, non-rarefied and merged phyloseq object ####
 load("../Diversity_Metrics_Analysis/filtered_phyloseq.RData")
 
-#### Perform Indicator Species Analysis on each of the 9 groups selected####
-## The nine groups selected were: us_control v.s us_tobacco, us_control v.s us_e-cigarette, cn_control v.s cn _tobacco, 
-## cn_control v.s cn_e-cigarette, us_control v.s cn_control, us_tobacco v.s cn_tobacco, us_e-cigarette v.s cn_e-cigarette, 
-## us_e-cigarette v.s us_tobacco, cn_e-cigarette v.s cn_tobacco ##
+#### Perform Indicator Species Analysis on each of the 6 categories ####
+## The six categories are: us_control, us_tobacco, us_e-cigarettes, cn_control, cn_tobacco and cn_e-cigarettes
 
 # Step 1: glom to Genus level while keeping the NAs
 merged_phyloseq_genus <- tax_glom(merged_phyloseq_filtered, "Genus", NArm = FALSE)
 # Step 2: Convert the counts in the OTU table to relative abundance
 merged_phyloseq_genus_RA <- transform_sample_counts(merged_phyloseq_genus, fun=function(x) x/sum(x))
 
-# Step 3: Make a list of our comparison groups
-comparisons <- list(
-  c("us_control","us_tobacco"),
-  c("us_control","us_ecigarettes"),
-  c("cn_control","cn_tobacco"),
-  c("cn_control","cn_ecigarettes"),
-  c("us_control","cn_control"),
-  c("us_tobacco","cn_tobacco"),
-  c("us_ecigarettes","cn_ecigarettes"),
-  c("us_ecigarettes","us_tobacco"),
-  c("cn_ecigarettes","cn_tobacco")
-)
+# Step 3: Run ISA using the Combined column
+isa_merged_phyloseq <- multipatt(t(otu_table(merged_phyloseq_genus_RA)), cluster = sample_data(merged_phyloseq_genus_RA)$`Combined`)
+summary(isa_merged_phyloseq) #view results
 
-# Step 4: Run Automated ISA for All Pairwise Comparisons
-results <- list()   # initialize the list to store output
+# Step 4: Convert taxonomy to table with ASV IDs
+taxtable <- tax_table(merged_phyloseq_filtered) %>% as.data.frame() %>% rownames_to_column(var="ASV")
 
-for(i in seq_along(comparisons)){
-  
-  pair <- comparisons[[i]]
-  
-  # subset to the two groups (use Combined column)
-  pair_ps <- subset_samples(merged_phyloseq_genus_RA, Combined %in% pair)
-  
-  # extract OTU table
-  otu <- as.data.frame(t(otu_table(pair_ps)))
-  # correct grouping variable (MUST match subset)
-  grp <- sample_data(pair_ps)$Combined
-  
-  # Run ISA
-  res <- multipatt(otu, grp, func = "IndVal.g", control = how(nperm = 999))
-  
-  # store summary in results list
-  results[[paste(pair, collapse = "_vs_")]] <- res
-}
+# Step 5: Make final table by joining the indicator ASVs back to their taxonomy to interpret the results biologically.
+final_table <- isa_merged_phyloseq$sign %>%
+  rownames_to_column(var="ASV") %>% #Convert the ASV from row names into a separate column
+  left_join(taxtable) %>% #Join with taxa table based on the ASVs ID
+  filter(p.value<0.05) #Filter for anything that has a p value of <0.05 
 
-# View results
-names(results)
+# Step 6: Save the table in the working directory
+write.csv(final_table, file = "final_table.csv", row.names = FALSE)
 
-
-# Step 5: Convert taxonomy to table with ASV IDs
-taxtable <- tax_table(merged_phyloseq_filtered) %>% as.data.frame() %>% rownames_to_column(var="ASV") #convert the ASV from row names into a separate column
-
-# Step 6: Combine all res$sign tables from the list
-all_results <- map_df(names(results), function(name){
-  results[[name]]$sign %>%
-    as.data.frame() %>%
-    rownames_to_column("ASV") %>% #convert the ASV from row names into a separate column
-    mutate(Comparison = name)
-})
-
-#Step 7: Make a final table by joining the indicator ASVs back to their taxonomy to interpret the results biologically
-final_table <- all_results %>%
-  left_join(taxtable, by = "ASV") %>% #join with taxa table based on the ASVs ID 
-  filter(p.value < 0.05) ##Filter for anything that has a p value of <0.05 
-
-#Reorder the columns in the existing final_table for better visualization
-final_table <- final_table %>%
-  relocate(s.us_ecigarettes,s.cn_control, s.cn_tobacco, s.cn_ecigarettes,
-           .before = index)
-
-# View final result
-View(final_table)
 
